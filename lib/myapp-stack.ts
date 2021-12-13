03f35f2264d27bb4c795d267f2ac79046b6cbecd
3l4l5 <csru20003@g.nihon-u.ac.jp>
2021-12-13 09:00:10 +0900

    first commit

--- lib/myapp-stack.ts
+++ lib/myapp-stack.ts
@@ -1,16 +1,19 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambdaNodejs from '@aws-cdk/aws-lambda-nodejs'
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as path from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class MyappStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class MyappStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyappQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // auto-make-draftのLambda Functionの作成
+    const fn = new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '..', 'lambda')),
+    });
   }
 }
